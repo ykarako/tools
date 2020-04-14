ecab1bfe0666dec0240cd676e9224d2015e4ae12
ykarako <karako.yukihisa@inaho.co>
2020-04-14 14:40:54 +0900

    add copy_pickle_log.sh

---
+++ copy_pickle_log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -evx
+
+pickle_dir="$HOME/inaho_robo/output/aspara_detector/recognition"
+config_dir="$HOME/inaho_robo/config/aspara_detector"
+tmp_dir="/tmp/copy_pickle_log/$(date '+%Y-%m-%d')"
+
+
+echo "pickleデータとzense設定ファイルのコピーを行います"
+
+mkdir -p $tmp_dir
+cp -r $pickle_dir/* $tmp_dir
+cp $config_dir/*.toml $tmp_dir
+
+cat << EOS > $tmp_dir/README.md
+圃場          ：xx
+ハウス番号    ：xx
+ロボ          ：P3-#xx
+ロボ運用担当者：xx
+撮影時間      ：xx時 - xx時
+天候          ：xx ［晴れ | 曇り | 雨］
+EOS
+
+echo "コピーが完了しました"
+echo "次のファイルのxxに各種情報を記載してね (・ω<)"
+echo $tmp_dir/README.md
+
